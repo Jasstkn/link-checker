5ee2eea643edd81da8b6f32a13e63e618b83ad60
Jasstkn <mariia.kotliarevskaia@gmail.com>
2022-08-10 18:13:24 +0200

    fix tests
    Signed-off-by: Jasstkn <mariia.kotliarevskaia@gmail.com>

--- pkg/linkchecker/linkchecker.go
+++ pkg/linkchecker/linkchecker.go
@@ -4,51 +4,10 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"regexp"
 	"strings"
-	"sync"
+
+	"github.com/Jasstkn/link-checker/internal/utils"
 )
-
-func ParseHtml(body string) []string {
-	re := regexp.MustCompile(`<a href="(http[a-zA-Z\d\-_.:\/]*?)"`)
-	matched := re.FindAllStringSubmatch(body, -1)
-
-	links := make([]string, 0, len(matched))
-	for _, v := range matched {
-		links = append(links, v[1])
-	}
-	return links
-}
-
-func ValidateLinks(links []string) (int, []string) {
-	var wg sync.WaitGroup
-	ch := make(chan string)
-
-	for _, l := range links {
-		wg.Add(1)
-		go func(l string) {
-			defer wg.Done()
-			r, _ := http.Get(l)
-			if r.StatusCode > 299 {
-				ch <- l
-			}
-		}(l)
-	}
-
-	// close chanel to prevent memory leak
-	go func() {
-		// wait until counter is 0
-		wg.Wait()
-		close(ch)
-	}()
-
-	var brokenLinks []string
-	for l := range ch {
-		brokenLinks = append(brokenLinks, l)
-	}
-
-	return len(brokenLinks), brokenLinks
-}
 
 func LinkChecker(url string) (string, error) {
 	req, err := http.Get(url)
@@ -61,13 +20,13 @@ func LinkChecker(url string) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	links := ParseHtml(string(body))
+	links := utils.ParseHtml(string(body))
 
 	if len(links) == 0 {
 		return "No links were found", nil
 	}
 
-	brokenNum, brokenLinks := ValidateLinks(links)
+	brokenNum, brokenLinks := utils.ValidateLinks(links)
 
 	switch {
 	case len(links) == 1 && brokenNum == 0:
